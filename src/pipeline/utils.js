f948060620debd0111ea1b95480f28706afc2dea
Tim Kurvers <tim@moonsphere.net>
2021-02-08 23:10:22 +0100

    feat: add stripIndent utility

--- src/pipeline/utils.js
+++ src/pipeline/utils.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/prefer-default-export */
-
 import path from 'path';
 
 export const changeExtension = (fqpath, extension) => {
@@ -10,4 +8,23 @@ export const changeExtension = (fqpath, extension) => {
     return path.join(dirname, `${basename}${extension}`);
   }
   return fqpath;
+};
+
+export const stripIndent = (strings, ...params) => {
+  let source = strings.map((string, i) => (
+    `${string}${params[i] || ''}`
+  )).join('');
+
+  // See: https://github.com/zspecza/common-tags/blob/master/src/stripIndentTransformer/stripIndentTransformer.js
+  const match = source.match(/^[^\S\n]*(?=\S)/gm);
+  const indent = match && Math.min(...match.map((el) => el.length));
+  if (indent) {
+    const regexp = new RegExp(`^.{${indent}}`, 'gm');
+    source = source.replace(regexp, '');
+  }
+
+  // Strip leading whitespace and trailing tabs/spaces
+  source = source.replace(/^\n+|[ \t]+$/g, '');
+
+  return source;
 };
